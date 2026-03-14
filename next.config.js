9766290c77fb7a1f6462cf040cf4c4d96f0872e3
copdowncow <parampapam755@gmail.com>
2026-03-15 01:57:31 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  // output: 'standalone' not needed - express custom server handles it
 };
 module.exports = nextConfig;
